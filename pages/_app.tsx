d084fbd19f660e48820e765bb25f1960fd409ffe
sanjay.rathod.mi@gmail.com <Sanjay@120297>
2021-02-12 17:43:49 +0530

    PWA Changes SW Service worker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
   ApolloProvider,
   ApolloClient,
@@ -31,7 +31,23 @@ interface Props {
 class App extends NextApp<Props> {
   render(): JSX.Element {
     const { Component, pageProps, apollo } = this.props
-
+    useEffect(() => {
+      if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function () {
+          navigator.serviceWorker.register('/sw.js').then(
+            function (registration) {
+              console.log(
+                'Service Worker registration successful with scope: ',
+                registration.scope,
+              )
+            },
+            function (err) {
+              console.log('Service Worker registration failed: ', err)
+            },
+          )
+        })
+      }
+    }, [])
     return (
       <ApolloProvider client={apollo}>
         <ChakraProvider>
