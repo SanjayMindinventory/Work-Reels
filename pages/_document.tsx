de796f824952d0e52990bbbc64576fc9e3bb53ed
sanjay.rathod.mi@gmail.com <Sanjay@120297>
2021-02-12 15:26:45 +0530

    PWA Changes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,9 @@
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 import React from 'react'
 
+const APP_NAME = 'next-pwa example'
+const APP_DESCRIPTION = 'This is an example of using next-pwa plugin'
+
 export default class Document extends NextDocument {
   render(): JSX.Element {
     return (
@@ -12,6 +15,39 @@ export default class Document extends NextDocument {
             rel='stylesheet'
           />
           <link href='//vjs.zencdn.net/7.10.2/video-js.css' rel='stylesheet' />
+          <meta name='application-name' content={APP_NAME} />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta
+            name='apple-mobile-web-app-status-bar-style'
+            content='default'
+          />
+          <meta name='apple-mobile-web-app-title' content={APP_NAME} />
+          <meta name='description' content={APP_DESCRIPTION} />
+          <meta name='format-detection' content='telephone=no' />
+          <meta name='mobile-web-app-capable' content='yes' />
+          <meta name='theme-color' content='#FFFFFF' />
+          {/* TIP: set viewport head meta tag in _app.js, otherwise it will show a warning */}
+          {/* <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' /> */}
+
+          <link
+            rel='apple-touch-icon'
+            sizes='180x180'
+            href='/icons/apple-touch-icon.png'
+          />
+          <link rel='manifest' href='/manifest.json' />
+          <link rel='shortcut icon' href='/icons/favicon.ico' />
+          <style>{`
+            html, body, #__next {
+              height: 100%;
+            }
+            #__next {
+              margin: 0 auto;
+            }
+            h1 {
+              text-align: center;
+            }
+            `}</style>
+          <title>Work Reels</title>
         </Head>
         <script
           dangerouslySetInnerHTML={{
